dcaec32b5712a9e356c0fcc5d5b0f633a61e2c9f
susanaviruglio <>
2023-08-04 16:39:30 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame(){
+
+}
+
+function checkAnwser(){
+
+
+}
+
+function calculateCorrectAnwser(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnwser(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractionQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
