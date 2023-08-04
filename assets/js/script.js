7ddc99e450b52ba7098c2e8127bd6e7ef085ad49
susanaviruglio <>
2023-08-04 17:10:38 +0000

    add event listener for document loading and button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+    /*for (i=0; i < buttons.length; i++) {
+we could have used this but we won´t use it
+} we are going to use an modern version syntax of it,
+ what it's going  to do is go through our buttons array and return  */
+document.addEventListener("DOMContentLoaded",function(){
+    let buttons = document.getElementsByTagName("button");
+
+     for (let button of buttons){
+        document.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+     }
+})
 function runGame(){
 
 }
@@ -28,5 +46,5 @@ function displaySubtractionQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }
