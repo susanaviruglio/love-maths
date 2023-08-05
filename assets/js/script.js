aab1ed64772933704499ae794a48f62de627ab35
susanaviruglio <>
2023-08-05 10:23:00 +0000

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,71 @@
-    /**for (i=0; i < buttons.length; i++) {
-*we could have used this but we won´t use it
-*} we are going to use an modern version syntax of it,
- what it's going  to do is go through our buttons array and return 
-*/
-document.addEventListener("DOMContentLoaded",function(){
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
-     for (let button of buttons){
-        document.addEventListener("click", function(){
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
-        })
-     }
-})
+        });
+    }
+
+    runGame("addition");
+
+});
+
 /**
  * The main game "loop", called when the script is first loaded
- * and after the user's answer has been processed 
+ * and after the user's answer has been processed
  */
+function runGame(gameType) {
 
-function runGame(){
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-    // creates two random numbers between 1 and 25
-  let num1 = Math.floor(Math.random() * 25) + 1;
-  let num2 = Math.floor(Math.random() * 25) + 1;
-}
-
-function checkAnswer(){
-
-
-}
-
-function calculateCorrectAnswer(){
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
-function incrementScore(){
+function checkAnswer() {
 
 }
 
-function incrementWrongAnwser(){
+function calculateCorrectAnswer() {
 
 }
 
-function displayAdditionQuestion(){
+function incrementScore() {
 
 }
 
-function displaySubtractionQuestion(){
+function incrementWrongAnswer() {
 
 }
 
-function displayMultiplyQuestion(){
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
 
 }
